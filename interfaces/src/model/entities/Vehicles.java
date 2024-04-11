f82d5af1214d04c540ee8404dbe9c1f7fd535755
Josaf4 <josafa.soares2909@gmail.com>
2024-04-11 16:17:12 -0400

    101
    1010

---
+++ interfaces/src/model/entities/Vehicles.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+public class Vehicles {
+
+	public static void main(String[] args) {
+		private String model;
+		
+		public Vehicles() {
+			
+		}
+		
+		public Vehicles(String model) {
+			this.model = model;
+		}
+		
+		public String getModel() {
+			return model;
+		}
+		
+		public String setModel(String model) {
+			this.model = model;
+		}
+}
